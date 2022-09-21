b41f37226b1bf65ab4e6df41bcdd3680af65c3ec
Elena <lelya2402@rambler.ru>
2022-09-21 20:54:40 +0300

    lecture

---
+++ lecture/hello.py
@@ -0,0 +1,32 @@
+# типы данных и переменная
+#value=None
+#print(type(value))
+
+#a=123
+#b=1.23
+#print(type(a))
+#print(type(b))
+#value=12334
+
+
+#print(type(value))
+
+s='hello world' # \n - переход на новую строку s='hello \nworld'
+a=123
+b=1.23
+
+print(s) # вывод строки
+print(a, ' - ',b,'-',s)
+print('{} - {} - {}'.format(a,b,s))
+print(f'{a} - {b} - {s}')
+
+
+f=True
+print(f)
+
+
+list=[]
+list=[1,2,3,4]
+list=['1','2','3']
+
+print(list)
