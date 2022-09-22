caf9c1b558a217e76ce737fcc8825438252348ed
Elena <lelya2402@rambler.ru>
2022-09-22 22:20:53 +0300

    task5

--- seminar/sem_1/task5.py
+++ seminar/sem_1/task5.py
@@ -8,4 +8,4 @@ num=int(input())
 #else:
 #    print("не кратно")
 
-print(bool(num%5==0 and num%10==0) or num %15==0) and num%30 !=0:
+print(bool(num%5==0 and num%10==0) or num %15==0) and num%30 !=0
