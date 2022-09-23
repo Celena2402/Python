7a3cf8c9174df3b51e45977b8325a470135def5d
Elena <lelya2402@rambler.ru>
2022-09-23 10:48:45 +0300

    task 3

--- seminar/sem_1/task3.py
+++ seminar/sem_1/task3.py
@@ -3,3 +3,15 @@
 #     *Примеры:*
 #     - 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 
+#number=int(input("Введите А: "))
+
+#for i in range(-a,a):
+#    print (i)
+
+#for value in range(-number, number+1):
+#    print(value, end=', ')
+
+# второй вариант
+number=int(input())
+
+print(*range(-number,number+1),sep=',')
