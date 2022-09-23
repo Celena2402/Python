5a2a9210ad88110f0c7fc64308360a73805a8508
Elena <lelya2402@rambler.ru>
2022-09-23 12:23:22 +0300

    task5

--- DZ/sem_1/task5.py
+++ DZ/sem_1/task5.py
@@ -4,3 +4,16 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
+print('Введите координаты первой точки А')
+xa=int(input('x= '))
+ya=int(input('y= '))
+
+print('Введите координаты второй точки B')
+xb=int(input('x= '))
+yb=int(input('y= '))
+
+#double result=Math.Sqrt((userX2-userX1)*(userX2-userX1)+(userY2-userY1)*(userY2-userY1));
+#result= Math.Round(result,2); // округление значения
+#Console.WriteLine($"Расстояние между точками {userX1}:{userY1} и {userX2}:{userY2} равно {result} ");
+
+result=
