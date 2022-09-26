49a1304fc99785b2ba08fcd97a1d417b914cea2b
Elena <lelya2402@rambler.ru>
2022-09-26 21:58:28 +0300

    task4

--- DZ/sem_1/task4.py
+++ DZ/sem_1/task4.py
@@ -1,35 +1,19 @@
 # 4. Напишите программу, которая по заданному номеру четверти, 
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
-Voit getCoordinateFromQuoter (int quoter)
-{
-    if (quoter==1)
-    {
-        Console.WriteLine($"Возможные координаты X>0 и Y>0");
-    }
-    else if (quoter==2)
-    {
-        Console.WriteLine($"Возможные координаты X<0 и Y>0");
-    }
-    else if (quoter==3)
-    {
-        Console.WriteLine($"Возможные координаты X<0 и Y<0");
-    }
-    else if (quoter==4)
-    {
-        Console.WriteLine($"Возможные координаты X>0 и Y<0");
-    }
-    else 
-    {
-        //Console.ForegroundColor=ConsoleColor.Red;
-        Console.WriteLine($"ОШИБКА: Введите номер четверти от 1 до 4!");
-    }
-}
-//int userQuoter=0;
+print('Введите номер четверти')
+n=int(input('n = '))
 
-Console.WriteLine("Введите номер четверти:");
-int userQuoter=Convert.ToInt32(Console.ReadLine());
-
-//int userQuoter=
-getCoordinateFromQuoter(userQuoter);
-//Console.WriteLine($"Возможные координаты для четверти {quoter} находятся в {quoter} четверти");
+if n==1:
+    print('Возможные координаты X>0 и Y>0')
+    
+elif n==2:
+    print('Возможные координаты X<0 и Y>0')
+    
+elif n==3:
+    print('Возможные координаты X<0 и Y<0')
+    
+elif n==4:
+    print('Возможные координаты X>0 и Y<0')
+else:
+    print("ОШИБКА: Введите номер четверти от 1 до 4!")
