124208d6a96a51d73e35a871d23e9991e42d741a
Elena <lelya2402@rambler.ru>
2022-09-28 17:05:59 +0300

    task1

--- DZ/sem_2/task1.py
+++ DZ/sem_2/task1.py
@@ -2,13 +2,19 @@
 #      Пример: 6782 -> 23; 0,56 -> 11
 
 num = float(input('Введите число N: '))
+#leng_num = len(str(num))
+#number = num * 10 ** (leng_num - 2)
+#print(sum([int(i) for i in list(str(int(number)))]))
+
+print(sum([int(i) for i in list(str(int(num * 10 ** (len(str(num))-2))))]))
+
 #number = num * 10 ** 2
-leng_num = len(str(num))
-number = num * 10 ** (leng_num - 2)
+#leng_num = len(str(num))
+#number = num * 10 ** (leng_num - 2)
 
-sum = 0
-while (number > 0):
-    sum = sum + number % 10
-    number = number // 10
+#sum = 0
+#while (number > 0):
+#    sum = sum + number % 10
+#    number = number // 10
 
-print('Сумма чисел у числа', num, 'равна', sum)
+#print('Сумма чисел у числа', num, 'равна', sum)
