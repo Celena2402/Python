9530724e303b02f87bfe6c14099f8a24fd28ab97
Elena <lelya2402@rambler.ru>
2022-09-28 10:13:39 +0300

    task1

--- DZ/sem_2/task1.py
+++ DZ/sem_2/task1.py
@@ -1,2 +1,14 @@
 # 1.  Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр. 
 #      Пример: 6782 -> 23; 0,56 -> 11
+
+num = float(input('Введите число N: '))
+#number = num * 10 ** 2
+leng_num = len(str(num))
+number = num * 10 ** (leng_num - 2)
+
+sum = 0
+while (number > 0):
+    sum = sum + number % 10
+    number = number // 10
+
+print('Сумма чисел у числа', num, 'равна', sum)
