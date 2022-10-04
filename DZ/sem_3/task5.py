579f9678813fdccd369ee1cb22a9ab54cc3004b7
Elena <lelya2402@rambler.ru>
2022-10-04 11:57:29 +0300

    task 3,4,5

--- DZ/sem_3/task5.py
+++ DZ/sem_3/task5.py
@@ -3,3 +3,25 @@
 #     Пример: - для k = 8 список будет выглядеть так: 
 #     [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 
+n = int(input('Введите количество элементов в списке: '))
+
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+    
+def NeoFibonacci(n):    
+    a, b = 1, -1
+    for i in range(n):
+        yield a
+        a,b=b,a-b
+    
+data0=[0]
+data = list(fibonacci(n))
+neo_data=list(NeoFibonacci(n))
+#print(data)
+#print(neo_data)
+print(f'Спиок чисел Негафибоначчи при n={n}:\n{neo_data[::-1]+data0+data}')
+
+
