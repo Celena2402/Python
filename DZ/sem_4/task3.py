1281203b03718230c776b8a4e7f0119ce9914091
Elena <lelya2402@rambler.ru>
2022-10-06 17:07:20 +0300

    task 3,4

--- DZ/sem_4/task3.py
+++ DZ/sem_4/task3.py
@@ -3,3 +3,15 @@
 #       которая выведет список неповторяющихся элементов 
 #       исходной последовательности.
 
+data=list(map(int, input('Введите последовательность чисел через пробел ').split()))
+
+new_data=[]
+
+for i in data:
+    if i not in new_data:
+        new_data.append(i)
+
+print(new_data)
+
+
+
--- DZ/sem_4/task4.py
+++ DZ/sem_4/task4.py
@@ -5,3 +5,32 @@
 #           Пример:
 #           - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+
+import random
+import itertools
+
+k=int(input('Введите степень многочлена k= '))
+odds = [random.randint(0, 100) for i in range(k+1)]
+print(odds)
+
+
+def getPolynomial(k, odds):
+    template = ['*x^'] * (k - 1) + ['*x']
+    #items=['+','-']
+    polynomial = [[a, b, c] for a, b, c in itertools.zip_longest(odds, template, range(k, 1, -1), fillvalue='') if a != 0]
+    for x in polynomial:
+        x.append(' + ') 
+        #x.append(items)
+    polynomial = list(itertools.chain(*polynomial))
+    polynomial[-1] = ' = 0'
+    return "".join(map(str, polynomial)).replace(' 1*x', ' x')
+
+
+#k = randint(2, 7)
+polynom = getPolynomial(k, odds)
+print(f'Многочлен степени {k} : {polynom}')
+
+with open('DZ/sem_4/task_4_1.txt', 'w') as data:
+    data.write(polynom)
+
+
