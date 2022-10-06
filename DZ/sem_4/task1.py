6df54cbede0d13dd1c4ffb67effeb4c70e46cc74
Elena <lelya2402@rambler.ru>
2022-10-06 15:16:02 +0300

    task 1,2

--- DZ/sem_4/task1.py
+++ DZ/sem_4/task1.py
@@ -1,4 +1,12 @@
-# Задача 1. 
+# Задача 1.
 #       Вычислить число c заданной точностью d
 #       Пример: - при d = 0.001, π = 3.141,    10^(-1) ≤ d ≤10^(-10)
 
+from decimal import Decimal
+n = Decimal(input('Введите вещественное число N= '))
+d = Decimal(input('Введите точно сть d= '))
+
+number = n.quantize(Decimal(d))
+print(number)
+
+print(f'Если число {n} округлить с точностью {d}, то получится число {number}')
--- DZ/sem_4/task2.py
+++ DZ/sem_4/task2.py
@@ -2,3 +2,21 @@
 #      Задайте натуральное число N. Напишите программу, которая 
 #       составит список простых множителей числа N.
 
+import math
+
+num = int(input('Введите натрульное число N= '))
+
+prime_factors = []
+n = num
+while n % 2 == 0:
+    prime_factors.append(2)
+    n = n//2
+
+for i in range(3, int(math.sqrt(n))+1, 2):
+    while (n % i == 0):
+        prime_factors.append(i)
+        n = n//i
+
+#print(prime_factors)
+print(f'Число {num} можно разложить на множители {prime_factors}')
+#print('{} = {}' .format(num, prime_factors))
