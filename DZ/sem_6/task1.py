54260dd7eeaedb8529c6103ebf742546ddad94b4
Elena <lelya2402@rambler.ru>
2022-10-13 21:50:25 +0300

    sem6

--- DZ/sem_6/task1.py
+++ DZ/sem_6/task1.py
@@ -1,9 +1,70 @@
 # Задача 1
-#       Напишите программу вычисления арифметического выражения заданного строкой. 
+#       Напишите программу вычисления арифметического выражения заданного строкой.
 #       Используйте операции +,-,/,*. приоритет операций стандартный.
 #       Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 #       Добавьте возможность использования скобок, меняющих приоритет операций.
 #       Пример: 1+2*3 => 7; (1+2)*3 => 9;
 
-data=list(map(int, input().split()))
-print(data)
+
+import math
+
+# 1.
+num = eval(input('Введите выражение: '))
+print (f'Результат выражения равен = {num}')
+
+# # 2.
+# func = {
+#     "^": lambda x, y: str(float(x) ** float(y)),
+#     "*": lambda x, y: str(float(x) * float(y)),
+#     "/": lambda x, y: str(float(x) / float(y)),
+#     "+": lambda x, y: str(float(x) + float(y)),
+#     "-": lambda x, y: str(float(x) - float(y))
+# }
+
+
+# def parenthesis_expansion(line_example): # раскрытие скобок, разбитие выражения
+#     new_lst = []                             #    
+#     i = 0
+#     while i < len(line_example):
+#         if line_example[i] == '(':
+#             n = line_example.index(')', i)
+#             new_lst.append(line_example[i + 1:n])
+#             i = n
+#         else:
+#             new_lst.append(line_example[i])
+#         i += 1
+#     return new_lst
+
+
+# def solution_brackets(new_lst):  # решение в скобках
+#     for i in range(len(new_lst)):
+#         if isinstance(new_lst[i], list):         # проверка принадлежности
+#             a, b, c = parenthesis_expansion(new_lst[i])
+#             new_lst[i] = func[b](a, c)           # вычисляется в скобках
+#     return new_lst
+
+
+# def calculation(new_lst):                         # вычисление
+#     first_lst = [i for i, j in enumerate(new_lst) if j in '*/']      # enumerate()– узнаем сразу индекс элемента и его значение    
+#     while first_lst:
+#         t = first_lst[0]
+#         a, b, c = new_lst[t - 1: t + 2]
+#         new_lst.insert(t - 1, func[b](a, c))
+#         del new_lst[t: t + 3]                             # удаление переменной из списка
+#         first_lst = [i for i, j in enumerate(new_lst) if j in '*/']
+     
+#     while len(new_lst) > 1:
+#         a, b, c = new_lst[ :3]
+#         del new_lst[ :3]
+#         new_lst.insert(0, func[b](a, c))
+#     return new_lst
+
+# example =input("Введите выражение через пробел: ")
+# #example = '3 + ( 9 - 6 ) * ( 8 - 4 ) + 8 / 2'     # результат 19
+# #print(example)
+# #print(parenthesis_expansion(example.split()))
+# #print(solution_brackets(parenthesis_expansion(example.split())))
+# res=calculation(solution_brackets(parenthesis_expansion(example.split())))
+# print(f'Результат выражения равен= {str(res)[2:-2].replace(",", "")}')
+
+
--- DZ/sem_6/task2.py
+++ DZ/sem_6/task2.py
@@ -3,3 +3,41 @@
 #       последовательности, список повторяемых и убрать дубликаты из заданной 
 #       последовательности.
 #       Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10] и [1, 3, 5] и [1, 2, 5, 3, 10]
+
+lst = [1, 2, 3, 5, 1, 5, 3, 10]
+print(lst)
+
+data=[x for x in lst]
+new_lst=list(filter(lambda x: lst.count(x)<2, data))
+print(new_lst)
+
+data_1=[x for x in set(lst)]
+new_lst1=list(filter(lambda x: lst.count(x)>1, data_1))
+print(new_lst1)
+
+new_lst2=[]
+new_lst2=list(filter(lambda x: x not in new_lst2, data_1))
+print(new_lst2)
+
+# ************
+# 2.
+
+# for i in lst:
+#     if lst.count(i)<2:
+#         new_lst.append(i)
+# print(new_lst)
+
+#  
+# new_lst1 = [x for x in set(lst) if lst.count(x) > 1]
+# # 2.
+# # new_lst1={}
+# # for i in lst:
+# #     if lst.count(i)>1:
+# #         new_lst1.append(i)
+# print(new_lst1)
+
+# new_lst2=[]
+# for i in lst:
+#     if i not in new_lst2:
+#         new_lst2.append(i)
+# print(new_lst2)
