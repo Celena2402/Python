3f4e8db5caa393e765d63c897aaae541185d05d1
Elena <lelya2402@rambler.ru>
2022-10-24 14:54:47 +0300

    task sem8

---
+++ DZ/sem_8/selection.py
@@ -0,0 +1,23 @@
+import menu
+import UI
+
+
+def start():
+    while True:
+        button=menu.menu()
+        if button == 1:
+            UI.add_view()
+        elif button == 2:
+            UI.save_view()
+        elif button == 3:
+            UI.output_view()  
+        elif button == 4:
+            UI.del_view()   
+        elif button == 5:
+            UI.editing_view()          
+        elif button == 0:
+            print('Выход')
+            exit()
+        else:
+            print('введите цифру из меню')
+            start()
